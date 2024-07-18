f307b69ff7e06d591537e321eee9d4a34f7acb0d
Merc1073 <sergeikoblov2000@gmail.com>
2024-07-18 19:12:29 +0100

    Player Crouch + 2D Colliders
    * Player can crouch, though it looks a little funny
    
    * Colliders switched from 3D to 2D. This includes the Capsule collider and the Cube floor colider.

--- Assets/Devs/Sergei Koblov/Scripts/PlayerMovement.cs
+++ Assets/Devs/Sergei Koblov/Scripts/PlayerMovement.cs
@@ -4,30 +4,51 @@ public class PlayerMovement : MonoBehaviour
 {
 
     [SerializeField] private float horizontal;
-    [SerializeField] private float speed = 8f;
+    [SerializeField] private float currentSpeed = 8f;
+    [SerializeField] private float originalSpeed = 8f;
     [SerializeField] private float jumpingPower = 16f;
     [SerializeField] private bool isFacingRight = true;
     [SerializeField] private bool isAirborne = false;
     [SerializeField] private bool hasJumped = false;
 
-
     [SerializeField] private int jumpCount;
     [SerializeField] private int maxJumpCount;
 
-    [SerializeField] private float fallingVelocity;
+    [SerializeField] private bool isCrouching = false;
+    [SerializeField] private Vector3 crouchScale;
+    [SerializeField] private Vector3 originalScale;
 
-    [SerializeField] private Rigidbody rb;
+    [SerializeField] private Rigidbody2D rb;
     [SerializeField] private Transform groundCheck;
     [SerializeField] private LayerMask groundLayer;
     [SerializeField] private float groundDistance = 0.4f;
     [SerializeField] private bool isGrounded;
 
+    private void Start()
+    {
+        originalScale = transform.localScale;
+        crouchScale = new Vector3(originalScale.x, originalScale.y * 0.5f, originalScale.z);
+    }
 
     private void Update()
     {
 
         CheckGrounded();
 
+        Movement();
+
+        Flip();
+        Crouch();
+
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(horizontal * currentSpeed, rb.velocity.y);
+    }
+
+    void Movement()
+    {
         horizontal = Input.GetAxisRaw("Horizontal");
 
         if (Input.GetButtonDown("Jump") && jumpCount != maxJumpCount)
@@ -36,7 +57,7 @@ public class PlayerMovement : MonoBehaviour
             rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
         }
 
-        if(hasJumped && isAirborne)
+        if (hasJumped && isAirborne)
         {
             jumpCount++;
             hasJumped = false;
@@ -57,14 +78,6 @@ public class PlayerMovement : MonoBehaviour
         {
             isAirborne = true;
         }
-
-        Flip();
-
-    }
-
-    private void FixedUpdate()
-    {
-        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
     }
 
     private void Flip()
@@ -78,9 +91,59 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
+    void Crouch()
+    {
+        if(Input.GetButton("Crouch"))
+        {
+            if(!isCrouching)
+            {
+                isCrouching = true;
+                float heightDifference = transform.localScale.y - crouchScale.y;
+                transform.position = new Vector3(transform.position.x, transform.position.y - heightDifference * 0.8f, transform.position.z);
+
+                if(isFacingRight)
+                {
+                    transform.localScale = crouchScale;
+                }
+
+                else if(!isFacingRight)
+                {
+                    Vector3 currentScale = crouchScale;
+                    currentScale.x *= -1f;
+                    transform.localScale = currentScale;
+                }
+
+                currentSpeed = originalSpeed * 0.5f;
+
+            }
+        }
+
+        else if(isCrouching)
+        {
+            isCrouching = false;
+            float heightDifference = transform.localScale.y - crouchScale.y;
+            transform.position = new Vector3(transform.position.x, transform.position.y + heightDifference * 0.8f, transform.position.z);
+
+            if(isFacingRight)
+            {
+                transform.localScale = originalScale;
+            }
+
+            else if (!isFacingRight)
+            {
+                Vector3 currentScale = originalScale;
+                currentScale.x *= -1f;
+                transform.localScale = currentScale;
+            }
+
+            currentSpeed = originalSpeed;
+
+        }
+    }
+
     void CheckGrounded()
     {
-        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundLayer);
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundDistance, groundLayer);
     }
 
     private void OnDrawGizmos()
